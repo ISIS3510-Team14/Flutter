114e53df7e5c3c33d0c77b04d7193f66527ccbdb
dcuevasa <d.cuevas@uniandes.edu.co>
2024-10-17 23:48:42 -0500

    Added changes for the data pipeline functionality, made design changes and order

--- lib/presentation/views/camera_screen.dart
+++ lib/presentation/views/camera_screen.dart
@@ -4,7 +4,6 @@ import '../widgets/camera_widget.dart';
 import 'package:camera/camera.dart';
 import '../widgets/head.dart';
 
-
 class CameraScreen extends StatelessWidget {
   final CameraDescription camera;
 
@@ -13,16 +12,13 @@ class CameraScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(title: const Text('Take a Picture')),
       body: Column(
         children: [
           SizedBox(height: 20),
           HeaderWidget(),
           Expanded(
             child: CameraWidget(camera: camera),
-            
           ),
-          
         ],
       ),
       bottomNavigationBar: BottomNavBar(currentIndex: 2),
