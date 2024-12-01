9e012fb9684d693aa498f6e2a994721d155384f8
Marilyn <m.joven@uniandes.edu.co>
2024-12-01 11:47:39 -0500

    History view

--- lib/presentation/views/history_screen.dart
+++ lib/presentation/views/history_screen.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../../core/utils/sustainu_colors.dart';
 import '../widgets/head.dart';
 import '../widgets/bottom_navbar.dart';
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends StatefulWidget {
+  @override
+  _HistoryScreenState createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  late DateTime _focusedDay;
+  DateTime? _selectedDay;
+
+  DateTime startDate = DateTime(2024, 11, 24);
+
+  @override
+  void initState() {
+    super.initState();
+    _focusedDay = DateTime.now();
+  }
+
+  bool _isStreakDay(DateTime day) {
+    final now = DateTime.now();
+    return day.isAfter(startDate.subtract(Duration(days: 1))) &&
+        day.isBefore(now.add(Duration(days: 1)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,30 +43,101 @@ class HistoryScreen extends StatelessWidget {
               child: HeaderWidget(),
             ),
             SizedBox(height: 20),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.calendar_month,
-                      size: 80, color: SustainUColors.limeGreen),
-                  SizedBox(height: 20),
-                  Text(
-                    'History View...',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: SustainUColors.text,
-                    ),
+            Text(
+              "Your Recycling History",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: SustainUColors.text,
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: TableCalendar(
+                firstDay: DateTime(2023, 1, 1),
+                lastDay: DateTime(2030, 12, 31),
+                focusedDay: _focusedDay,
+                selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                calendarStyle: CalendarStyle(
+                  todayDecoration: BoxDecoration(
+                    color: SustainUColors.limeGreen,
+                    shape: BoxShape.circle,
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    'coming soon to reveal your eco-friendly journey',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: SustainUColors.text,
-                    ),
+                  selectedDecoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
                   ),
-                ],
+                  defaultDecoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  outsideDaysVisible: false,
+                ),
+                daysOfWeekStyle: DaysOfWeekStyle(
+                  weekdayStyle: TextStyle(color: SustainUColors.text),
+                  weekendStyle: TextStyle(color: SustainUColors.text),
+                ),
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: false,
+                  titleTextStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: SustainUColors.text,
+                  ),
+                  leftChevronIcon:
+                      Icon(Icons.chevron_left, color: SustainUColors.text),
+                  rightChevronIcon:
+                      Icon(Icons.chevron_right, color: SustainUColors.text),
+                ),
+                calendarBuilders: CalendarBuilders(
+                  defaultBuilder: (context, day, focusedDay) {
+                    if (_isStreakDay(day)) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: SustainUColors.limeGreen,
+                          shape: BoxShape.circle,
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          '${day.day}',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    }
+                    return null;
+                  },
+                  todayBuilder: (context, day, focusedDay) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: SustainUColors.limeGreen,
+                        shape: BoxShape.circle,
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        '${day.day}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  },
+                  selectedBuilder: (context, day, focusedDay) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        '${day.day}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ],
